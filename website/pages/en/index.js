d0569f64ebcdc5c363cd783c16654711d798b2d4
Dimitrios Lytras <dnlytras@gmail.com>
2019-02-02 13:21:00 +0200

    init

---
+++ website/pages/en/index.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+const styled = require('styled-components').default;
+const { Button, Alert, Provider } = require('rendition');
+const {
+  Button: ButtonSC,
+  Alert: AlertSC,
+  ThemeProvider: ProviderSC,
+} = require('@smooth-ui/core-sc');
+
+const { theme } = require('@smooth-ui/core-sc');
+
+const Spacing = styled.div`
+  margin: 24px 0;
+`;
+
+const RenditionComponents = () => (
+  <Provider>
+    <div>
+      <h1>Rendition Components</h1>
+      <Button onClick={() => alert('works')}>Rendition default button</Button>
+      <Spacing />
+      <Button onClick={() => alert('works')} danger>
+        Rendition danger Button
+      </Button>
+      <Spacing />
+      <Alert>Rendition Alert</Alert>
+    </div>
+  </Provider>
+);
+
+const SmoothUICompoments = () => (
+  <ProviderSC theme={theme}>
+    <div>
+      <h1>Smooth UI Components</h1>
+      <ButtonSC onClick={() => alert('works')}>
+        Smooth UI default button
+      </ButtonSC>
+      <Spacing />
+      <ButtonSC onClick={() => alert('works')} variant="danger">
+        Smooth UI danger Button
+      </ButtonSC>
+      <Spacing />
+      <AlertSC>Smooth UI Alert</AlertSC>
+    </div>
+  </ProviderSC>
+);
+
+const Index = () => (
+  <div>
+    <RenditionComponents />
+    <br />
+    <SmoothUICompoments />
+  </div>
+);
+
+module.exports = Index;
